5bb8c628c14c456bffd4d05363696dd480662802
YanViniciusSilva <yanviniciusjesussilva@gmail.com>
2023-08-03 19:23:20 -0300

    :rocket: feat: add application header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './shell/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HeaderComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
