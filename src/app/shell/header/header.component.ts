025712e279d89c80ac61877bc405f065155338d3
YanViniciusSilva <yanviniciusjesussilva@gmail.com>
2023-08-06 21:40:27 -0300

    :rocket: feat: add social btns on header

--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -9,4 +9,28 @@ export class HeaderComponent {
 
   @Input()
   visibility:boolean = true
+
+  date = new Date();
+  month = this.date.getMonth();
+  day = this.date.getDay();
+  year = this.date.getFullYear();
+
+  socialMidiaLinks = [
+    {
+      link: 'https://linkedin.com/in/yan-vinicius-jesus-silva-b1b071238',
+      image: 'assets/linkedin.png'
+    },
+    {
+      link: 'https://github.com/YanViniciusSilva',
+      image: 'assets/git.png'
+    },
+    {
+      link: 'mailto:yanviniciusjesussilva@gmail.com',
+      image: 'assets/gmail.png'
+    }
+  ]
+
+  get myAge() {
+    return this.month == 9 && this.day == 23 ? this.year - 2003 : (this.year - 1) - 2003;
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      /* Animations */
+      keyframes: {
+        typing: {
+          'from': { width: '0px', 'border-right': '3px solid'},
+          'to': { 'border-right': '3px solid' }
+        },
+        blinking: {
+          '50%': { 'border-color': 'transparent' },
+        },
+        rotate: {
+          'from' : { transform: 'rotate(-10deg)' },
+          '25%' : { transform: 'rotate(0deg)' },
+          '50%' : { transform: 'rotate(10deg)' },
+          '100%' : { transform: 'rotate(0deg)' },
+        }
+      },
+      animation: {
+        'typing' : 'typing 3s steps(20), blinking 0.5s',
+        'rotate' : 'rotate 0.3s',
+      }
+    },
     fontFamily: {
       'ubuntu': ['Ubuntu', 'sans-serif'],
-    }
+    },
   },
   plugins: [],
 }
