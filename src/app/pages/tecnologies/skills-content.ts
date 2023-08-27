4eaaeaa3931184e528b4fe547b31e08ba7ce2a26
YanViniciusSilva <yanviniciusjesussilva@gmail.com>
2023-08-27 17:29:53 -0300

    :rocket: feat: change cards/theme styles and add header animation

--- src/app/pages/tecnologies/skills-content.ts
+++ src/app/pages/tecnologies/skills-content.ts
@@ -76,4 +76,124 @@ export const skillsContent:SkillContentModel[] = [
     monst_used: false,
     skill_area: EnumSkillArea.front
   },
+  {
+    title: 'Firebase',
+    description: `
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Molestias, ratione suscipit. Nesciunt quisquam velit
+        recusandae tempora voluptatem praesentium officia
+        reprehenderit unde. Eaque nisi dolores odio officiis
+        nemo ut libero earum.
+      </p>
+    `,
+    image: 'assets/sections-images/tecnologies-imgs/cards/firebase.svg',
+    monst_used: false,
+    skill_area: EnumSkillArea.extra
+  },
+  {
+    title: 'Java',
+    description: `
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Molestias, ratione suscipit. Nesciunt quisquam velit
+        recusandae tempora voluptatem praesentium officia
+        reprehenderit unde. Eaque nisi dolores odio officiis
+        nemo ut libero earum.
+      </p>
+    `,
+    image: 'assets/sections-images/tecnologies-imgs/cards/java.svg',
+    monst_used: false,
+    skill_area: EnumSkillArea.back
+  },
+  {
+    title: 'JavaScript',
+    description: `
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Molestias, ratione suscipit. Nesciunt quisquam velit
+        recusandae tempora voluptatem praesentium officia
+        reprehenderit unde. Eaque nisi dolores odio officiis
+        nemo ut libero earum.
+      </p>
+    `,
+    image: 'assets/sections-images/tecnologies-imgs/cards/javascript.svg',
+    monst_used: false,
+    skill_area: EnumSkillArea.back
+  },
+  {
+    title: 'Node JS',
+    description: `
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Molestias, ratione suscipit. Nesciunt quisquam velit
+        recusandae tempora voluptatem praesentium officia
+        reprehenderit unde. Eaque nisi dolores odio officiis
+        nemo ut libero earum.
+      </p>
+    `,
+    image: 'assets/sections-images/tecnologies-imgs/cards/node.svg',
+    monst_used: false,
+    skill_area: EnumSkillArea.back
+  },
+  {
+    title: 'RxJS',
+    description: `
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Molestias, ratione suscipit. Nesciunt quisquam velit
+        recusandae tempora voluptatem praesentium officia
+        reprehenderit unde. Eaque nisi dolores odio officiis
+        nemo ut libero earum.
+      </p>
+    `,
+    image: 'assets/sections-images/tecnologies-imgs/cards/rxjs.svg',
+    monst_used: false,
+    skill_area: EnumSkillArea.front
+  },
+  {
+    title: 'SQL Server',
+    description: `
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Molestias, ratione suscipit. Nesciunt quisquam velit
+        recusandae tempora voluptatem praesentium officia
+        reprehenderit unde. Eaque nisi dolores odio officiis
+        nemo ut libero earum.
+      </p>
+    `,
+    image: 'assets/sections-images/tecnologies-imgs/cards/sql.svg',
+    monst_used: false,
+    skill_area: EnumSkillArea.extra
+  },
+  {
+    title: 'Typescript',
+    description: `
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Molestias, ratione suscipit. Nesciunt quisquam velit
+        recusandae tempora voluptatem praesentium officia
+        reprehenderit unde. Eaque nisi dolores odio officiis
+        nemo ut libero earum.
+      </p>
+    `,
+    image: 'assets/sections-images/tecnologies-imgs/cards/typescript.svg',
+    monst_used: false,
+    skill_area: EnumSkillArea.front
+  },
+  {
+    title: 'Tailwind',
+    description: `
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Molestias, ratione suscipit. Nesciunt quisquam velit
+        recusandae tempora voluptatem praesentium officia
+        reprehenderit unde. Eaque nisi dolores odio officiis
+        nemo ut libero earum.
+      </p>
+    `,
+    image: 'assets/sections-images/tecnologies-imgs/cards/tailwind.svg',
+    monst_used: false,
+    skill_area: EnumSkillArea.front
+  },
 ];
