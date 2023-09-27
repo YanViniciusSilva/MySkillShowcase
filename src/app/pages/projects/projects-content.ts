a32016c25774f305714973e5754f5d2fe62a72f8
YanViniciusSilva <yanviniciusjesussilva@gmail.com>
2023-09-26 21:08:00 -0300

    :rocket: feat: add new projects

--- src/app/pages/projects/projects-content.ts
+++ src/app/pages/projects/projects-content.ts
@@ -11,16 +11,16 @@ export interface ProjectsContentModel {
 
 export const projectsContent:ProjectsContentModel[] = [
   {
-    title: 'Pokedex',
+    title: 'Troca Livro (TCC)',
     description: `
       Lorem ipsum, dolor sit amet consectetur adipisicing elit.
       Dolorum quibusdam fugiat commodi iure aspernatur pariatur,
       mollitia ad a excepturi similique officiis accusamus hic
       impedit illo reprehenderit reiciendis? Fuga, cum expedita?
     `,
-    image: 'assets/sections-images/projects-imgs/pokedex.svg',
+    image: 'assets/sections-images/projects-imgs/tradeBook.svg',
     project_url: 'www.pokedex.com',
-    project_git_url: 'github.com/YanViniciusSilva/PokedexAngular',
+    project_git_url: 'github.com/YanViniciusSilva/TradeBook',
     year: 2021,
     isPrivate: false,
     deployed: false
@@ -36,7 +36,37 @@ export const projectsContent:ProjectsContentModel[] = [
     image: 'assets/sections-images/projects-imgs/invite.svg',
     project_url: 'wedding-inviters-list.firebaseapp.com',
     project_git_url: 'github.com/YanViniciusSilva/Wedding-invite',
-    year: 2021,
+    year: 2022,
+    isPrivate: false,
+    deployed: true
+  },
+  {
+    title: 'Pokedex',
+    description: `
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Dolorum quibusdam fugiat commodi iure aspernatur pariatur,
+      mollitia ad a excepturi similique officiis accusamus hic
+      impedit illo reprehenderit reiciendis? Fuga, cum expedita?
+    `,
+    image: 'assets/sections-images/projects-imgs/pokedex.svg',
+    project_url: 'www.pokedex.com',
+    project_git_url: 'github.com/YanViniciusSilva/PokedexAngular',
+    year: 2023,
+    isPrivate: false,
+    deployed: false
+  },
+  {
+    title: 'Portifólio',
+    description: `
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Dolorum quibusdam fugiat commodi iure aspernatur pariatur,
+      mollitia ad a excepturi similique officiis accusamus hic
+      impedit illo reprehenderit reiciendis? Fuga, cum expedita?
+    `,
+    image: 'assets/sections-images/projects-imgs/portifolio.svg',
+    project_url: 'yansilvadev.com.br',
+    project_git_url: 'github.com/YanViniciusSilva/MySkillShowcase',
+    year: 2023,
     isPrivate: false,
     deployed: true
   }
